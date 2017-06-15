197eea76ae0d2cfaa5ec2a1759477d0cf0768e84
hesiyuan <leonardohe@kugou.net>
2017-06-15 14:56:15 +0800

    python the rc version modify

--- src/AutoCompile/FuncTool.py
+++ src/AutoCompile/FuncTool.py
@@ -29,22 +29,34 @@ def GetSlnPath():
 
 
 def SetRcVersion(tRcFile, TargetVersion):
-    with open(tRcFile, "w+") as f:
+    with open(tRcFile, "r+") as f:
         rc_content = f.read()
-        iStr = rc_content.decode('utf16')
+        iStr = rc_content.decode('GBK')
         regex1 = re.compile(ur"VALUE\s*\"FileVersion\"\s*\,\s*\"\d+\.\d+\.\d+\.\d+\"", re.MULTILINE)
-        iTarget = unicode.format(u"VALUE  \"FileVersion\" ,\"%s\"",TargetVersion)
+        iTarget = unicode.format(u"VALUE  \"FileVersion\" , \"%s\""%TargetVersion)
         pass_2 = re.sub(regex1, iTarget, iStr,1)
         print pass_2
-        # f.write(pass_2)
+        if pass_2 is not  None:
+            f.seek(0)
+            f.write(pass_2.encode('GBK'))
+            f.truncate()
 
 
 
-#SetRcVersion(r"C:/repo/projectA/resources/win/projectA.rc", "3,4,5,6")
+
+
+def TestRc():
+    iTarget = u"i am fine"
+    iReplace = u'-'
+    iRegex = re.compile(ur'\s')
+    iResult = re.sub(iRegex,iReplace,iTarget)
+    print iResult
+
 
 
 
 if __name__ == "__main__":
     # GetVersion('./test.rc')
     SetRcVersion('./test.rc' ,'4.22.0.141')
+    # TestRc()
     pass
