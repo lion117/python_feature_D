6d409216b598219f859aff2fa11495c011f2ccbe
hesiyuan <leonardohe@kugou.net>
2017-06-28 11:57:35 +0800

    add convert to exe code

---
+++ src/CatchThief/ToExe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2014/8/21
+@author: LEO
+"""
+
+from distutils.core import setup
+import py2exe
+
+
+
+setup(console=["VideoManager.py"],
+      options={"py2exe":{
+                  "compressed": True ,
+                  "optimize": 2,
+                  "bundle_files": 2 ,
+              }
+         })
+
+
